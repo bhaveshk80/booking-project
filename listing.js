74cc9a07714acc35f7e2ed689667c61aaeb4a1ed
bhaveshk80 <bkumkar6@gmail.com>
2025-02-27 19:55:55 +0530

    Add files via upload

--- listing.js
+++ listing.js
@@ -1,28 +1,38 @@
-const express = require("express");
-const router = express.Router();
-const Listing = require("../models/listing.js");
-const wrapAsync = require("../utils/wrapAsync.js");
-const {isLoggedIn} = require("../middleware.js");
-const {isOwner} = require("../middleware.js");
-const {validateListing} = require("../middleware.js");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
-const listingController = require("../controllers/listings.js");
-const multer  = require('multer');
-const {storage} = require("../cloudConfig.js");
-const upload = multer({storage});
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },   
+    description: String,
+    image: {
+       url:String,
+       filename: String,
+    },
+    price:Number,
+    location: String,
+    country: String,
+    reviews: [
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Review",
+        },
+    ],
+    owner : {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+});
 
-router.route("/")
-.get(wrapAsync(listingController.index))
-.post(isLoggedIn,upload.single('listing[image]'),validateListing,wrapAsync(listingController.createListing));
 
-router.get("/new", isLoggedIn,listingController.renderNewForm);
+listingSchema.post("findOneAndDelete",async(listing) => {
+    if(listing) {
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+    }
+});
 
-router.route("/:id")
-.get(wrapAsync(listingController.showListing))
-.put(isLoggedIn,isOwner,upload.single('listing[image]'),validateListing, wrapAsync(listingController.updateListing))
-.delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListing));
-
-//Edit route
-router.get("/:id/edit",isLoggedIn,isOwner, wrapAsync(listingController.renderEditform));
-
-module.exports = router;
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
